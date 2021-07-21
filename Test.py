165509fda7064e4d6410ea60df137bc0d5a29b06
Manny3307 <manmeetarora@gmail.com>
2021-07-21 21:15:52 +1000

    New Changes regarding creating folder structure

--- Test.py
+++ Test.py
@@ -10,3 +10,9 @@ print(data["configs"]["HTMLHeaderTemplate"])
 f.close()
 print(os.name)
 print(sys.platform)
+
+FolderConfigJSON = open('./Config/folder_config.json')
+ConfigData = json.load(FolderConfigJSON)
+
+HTMLFilestoCopy = ConfigData['folder_configs']['HTMLFiles']['HTMLFilesToCopy']['CleaningRecordHeader']
+print(f"Uber Cleaning Header Template = {HTMLFilestoCopy}")
