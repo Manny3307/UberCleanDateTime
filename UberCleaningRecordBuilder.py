e1285ba466871234cf5bd0841d10f2406965518d
Manny3307 <manmeetarora@gmail.com>
2021-07-31 20:46:41 +1000

    Added Logging in the Program

--- UberCleaningRecordBuilder.py
+++ UberCleaningRecordBuilder.py
@@ -1,3 +1,4 @@
+from IPython.core.display import ProgressBar
 from numpy.lib.function_base import append
 import pandas as pd
 import glob
@@ -10,7 +11,29 @@ import os, ntpath
 from shutil import copyfile
 from sqlalchemy import create_engine, orm
 import mysql.connector
+import traceback
 
+filename = './Logs/UberLog.json'
+now = dt.now()
+dtLog_string = now.strftime("%d/%m/%Y %H-%M-%S")
+
+UberLogData = {}
+dateString = {}
+
+UberLogString = [dtLog_string]
+
+def create_prog_log(logString):
+    progLog = logString
+
+    UberLogData["UberDateLog"] = dtLog_string
+    UberLogData["UberLogs"] = logString
+
+    with open(filename, "r+") as file:
+        data = json.load(file)
+        data.append(UberLogData)
+        file.seek(0)
+        json.dump(data, file, indent=1)
+    
 # Load the Config JSON file from the config folder and read the respective values
 FolderConfigJSON = open('./Config/folder_config.json')
 CreateConfigData = json.load(FolderConfigJSON)
@@ -50,6 +73,9 @@ Password = dbconf["DBConfigs"]["Password"]
 ServerOrEndPoint = dbconf["DBConfigs"]["ServerOrEndPoint"]
 DatabaseName = dbconf["DBConfigs"]["DatabaseName"]
 
+UberLogString.append("Configurations Loaded successfully")
+#create_prog_log("Configurations Loaded successfully")
+
 #Set the Upper and Lower Limit for Time Substraction
 lower_time_range = 2
 upper_time_range = 8 
@@ -63,6 +89,8 @@ folderName = input("Please Enter the Folder Name: ")
 dirName = CreateBasePath + folderName
 HTMLDirName = dirName + CreateHTMLFolder
 CSVDirName = dirName + CreateCSVFolder
+
+UberLogString.append("Creating folder successfully")
 
 # Create target Directory
 if not os.path.exists(dirName):
@@ -84,6 +112,8 @@ if not os.path.exists(dirName):
         print("Directory " , CSVDirName ,  " already exists")      
 else:
     print("Directory " , dirName ,  " already exists")   
+
+UberLogString.append("Folder Structure created successfully")
 
 BuildHTMLPath = BuildPath + CreateHTMLFolder
 BuildCSVPath = BuildPath + CreateCSVFolder
@@ -110,6 +140,8 @@ copy_files_to_dest_folder(HTMLTemplatefiles_src, HTMLDirName)
 #Copy the CSV Files 
 copy_files_to_dest_folder(CSVTemplatefiles_src, CSVDirName)
 
+UberLogString.append("CSV and HTML files copied successfully to the newly created folders")
+
 # Read a single CSV file or multiple CSV files from the Given Folder.
 myfiles = glob.glob(f"{BasePath}{folderName}{CSVFolder}*.csv")
 
@@ -127,6 +159,8 @@ if myfiles != None:
         
 # Get the Dump of Uber Date and Time data 
 UberTripData = pd.concat(TempUberTripDataList, axis=0, ignore_index=True)
+
+UberLogString.append("Date and Time Data successfully loaded in the dataframe")
 
 #Calculate the clean time for Uber Trips
 def UberSplitDateTime(UberDateTime, TimeInMinutes):
@@ -155,14 +189,19 @@ for (k, v) in dataconf.items():
     else:
         final_df[v["dfColumn"]] = v["Value"]
 
+UberLogString.append("Creating the final dataframe having all the required columns")
+
+UberLogString.append("Connecting to Database")
 # Create SQLAlchemy engine to connect to MySQL Database
 engine = create_engine(f"{DBConnector}://{UserName}:{Password}@{ServerOrEndPoint}/{DatabaseName}", encoding='utf8')
 
+UberLogString.append("Sending Records to UberTempCleaningRecords table in database....")
 print("Sending Records to database....")
 
 # Convert dataframe to sql table                                   
 final_df.to_sql('UberTempCleaningRecords', engine, if_exists='append', index=False)
 
+UberLogString.append("Sending Records to UberCleaningRecords table through InsertJSONCleaningRecord stored procedure")
 connection = engine.raw_connection()
 # define parameters to be passed in and out
 parameterIn = None
@@ -178,11 +217,18 @@ finally:
 
 #Print the message returned from the Stored Procedure
 print(results[0])
+UberLogString.append(results[0])
 
 print("Records successfully sent to database !!!")
+UberLogString.append("Records successfully sent to UberCleaningRecords in database !!!")
+
 
 #Update Column Names of the main Dataframe
 final_df.rename(columns=eval(DataFrameColumnValues), inplace = True)  
+
+UberLogString.append("Renaming the dataframe columns to one provided in the CPVV Template !!!")
+
+UberLogString.append("Rendering the dataframe to HTML!!!")
 
 #Convert data frame into a HTML table.
 BodyTemplate = final_df.to_html(classes='mystyle',index=False)
@@ -196,10 +242,19 @@ FooterTemplate=open(f"{BasePath}{folderName}{HTMLFolder}{HTMLFooterTemplate}").r
 #Concatenate all the templates into End Result to form one complete HTML
 EndResult = str(HeaderTemplate) + str(BodyTemplate) + str(FooterTemplate)
 
+UberLogString.append("Concatenating the final HTML!!!")
+
 #Assign EndResult to UberCleanTimeHTML.html file.
 UberDateTimeHTML = open(f"{BasePath}{folderName}{HTMLFolder}{FinalHTMLResult}","w")
 UberDateTimeHTML.write(EndResult)
 UberDateTimeHTML.close()
 
+UberLogString.append("Saving the final HTML to HTML file in HTML Folder!!!")
+
+UberLogString.append("Begin to write the PDF file from the resultant HTML!!!")
+
 #Create PDF from UberCleanTimeHTML.html to upload into Uber Portal.
 pdfkit.from_file(f"{BasePath}{folderName}{HTMLFolder}{FinalHTMLResult}", GetCurrentPathName())
+
+UberLogString.append("PDF written successfully from the resultant HTML file!!!")
+create_prog_log(UberLogString)
